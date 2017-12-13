cc582471f4925d06b372b4d564863d8b54ada542
Felix Millne <felix.millne@btinternet.com>
2017-12-13 20:24:58 +0000

    Require station to have been given route to station before hasRoute() returns true

--- src/main/java/millne/felix/trains/Station.java
+++ src/main/java/millne/felix/trains/Station.java
@@ -1,14 +1,24 @@
 package millne.felix.trains;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 /**
  * Created by FelixMarcus on 13/12/2017.
  */
 public class Station {
+    private final Set<Station> destinations = Sets.newHashSet();
+
     public boolean hasRoute(Station toStation) {
         if(toStation == null) {
             return false;
         }
 
-        return true;
+        return destinations.contains(destinations);
+    }
+
+    public void addRouteTo(Station destination) {
+        destinations.add(destination);
     }
 }
--- src/test/java/millne/felix/trains/TestStation.java
+++ src/test/java/millne/felix/trains/TestStation.java
@@ -12,14 +12,22 @@ import static org.junit.Assert.assertTrue;
 public class TestStation {
 
     @Test
-    public void testStationHasNoRoutes(){
+    public void testStationHasNoRouteToNullStation(){
         Station station = new Station();
         assertFalse(station.hasRoute(null));
     }
 
     @Test
-    public void testStationHasRoute(){
+    public void testStationHasNoRoute(){
         Station station = new Station();
-        assertTrue(station.hasRoute(new Station()));
+        assertFalse(station.hasRoute(new Station()));
+    }
+
+    @Test
+    public void testStationGivenRouteHasRoute(){
+        Station departureStation = new Station();
+        Station destination = new Station();
+        departureStation.addRouteTo(destination);
+        assertFalse(departureStation.hasRoute(new Station()));
     }
 }
