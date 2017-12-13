f3ae5edff739ffd5a0694d2162bb4390a67ad08c
Felix Millne <felix.millne@btinternet.com>
2017-12-13 20:56:50 +0000

    Add Javadoc comments to Station class

--- src/main/java/millne/felix/trains/Station.java
+++ src/main/java/millne/felix/trains/Station.java
@@ -13,8 +13,15 @@ import java.util.Set;
 public class Station {
 
     private final Map<Station, Integer> destinations = Maps.newHashMap();
+
     private final String name;
 
+    /**
+     *
+     * @param name - The name of station. Must not be null. Primary identifier for equality between stations.
+     *
+     * @throws IllegalArgumentException - on name value of @null
+     */
     public Station(String name) {
         if(Strings.isNullOrEmpty(name)){
             throw new IllegalArgumentException("Station name cannot be null");
@@ -22,6 +29,12 @@ public class Station {
         this.name = name;
     }
 
+    /**
+     * Determines if station has knowledge of a route to the destination station.
+     *
+     * @param toStation
+     * @return @true if the station has a route to the given destination station, else @false if not or if destination is null
+     */
     public boolean hasRouteTo(Station toStation) {
         if(toStation == null) {
             return false;
@@ -30,10 +43,22 @@ public class Station {
         return destinations.containsKey(toStation);
     }
 
+    /**
+     * Sets a new route to the given @destination with given @distance
+     *
+     * @param destination - Target station of new route
+     * @param distance - Distance to @destination
+     */
     public void addRouteTo(Station destination, int distance) {
         destinations.put(destination, distance);
     }
 
+    /**
+     * Retrieves the previously set distance to @destination
+     *
+     * @param destination
+     * @return
+     */
     public int getDistanceTo(Station destination) {
         return destinations.get(destination);
     }
